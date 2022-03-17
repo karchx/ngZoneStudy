9bf3b2dcafb6060b47cc2126b1610a093e38b208
KenethSandoval <kenethra.74@gmail.com>
2022-03-16 18:56:13 -0600

    feat: once test

--- backend/src/__tests__/server.spec.js
+++ backend/src/__tests__/server.spec.js
@@ -1,3 +1,25 @@
 import request from 'supertest';
-import { app } from '../server';
 import mongoose from 'mongoose';
+
+import { app } from '../server';
+import { User } from '../resources/user/user.model';
+
+describe('API Authentication:', () => {
+	beforeEach(async() => {
+		const user = await User.create({email: 'a@a.com', password: 'hello'});
+		console.log(user);
+	});
+
+	describe('api auth', () => {
+		test('api should be locked down', async() => {
+			let response = await request(app).get('/api/item');
+			expect(response.statusCode).toBe(401);
+
+			response = await request(app).get('/api/list');
+			expect(response.statusCode).toBe(401);
+
+			response = await request(app).get('/api/user');
+			expect(response.statusCode).toBe(401)
+		});
+	});
+});
