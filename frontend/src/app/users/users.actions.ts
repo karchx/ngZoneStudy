d6e316354f7f428d11a51f22681c9bc518b04607
Стив <kenethra.74@gmail.com>
2022-09-05 09:51:10 -0600

    feat: update actions ngrx

--- frontend/src/app/users/users.actions.ts
+++ frontend/src/app/users/users.actions.ts
@@ -1,13 +1,11 @@
-import { createAction, props } from "@ngrx/store";
-import { User } from "../core/models/user";
+import {createActionGroup, emptyProps, props} from "@ngrx/store";
+import {User} from "../core/models/user";
 
-export enum ActionsTypes {
-  SIGN_UP = "[SIGN_UP] Sign up",
-  SIGN_UP_SUCCESS = "[SIGN_UP] Sign up success"
-}
-
-export const signup = createAction(ActionsTypes.SIGN_UP, props<{ user: User }>());
-export const signupSuccess = createAction(
-  ActionsTypes.SIGN_UP,
-  props<{ user: User; authenticated: boolean }>()
-);
+export const authActions = createActionGroup({
+  source: 'Auth',
+  events: {
+    Register: emptyProps(),
+    'Register Failure': props<{ error: Error }>(),
+    'Register Success': props<{ user: User }>(),
+  }
+});
