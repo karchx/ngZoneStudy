c7f8578a9b92fea846c325715f94541ce3e8b4e0
Стив <kenethra.74@gmail.com>
2022-09-06 21:47:17 -0600

    feat(frontend): state rehydrate

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 
 import { EffectsModule } from "@ngrx/effects";
-import { StoreModule } from "@ngrx/store";
+import { ActionReducer, MetaReducer, StoreModule } from "@ngrx/store";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { ToastrModule } from "ngx-toastr";
 
@@ -13,10 +13,18 @@ import { AppRoutingModule } from "./app-routing.module";
 import { NebularModule } from "./nebular/nebular.module";
 
 import { AppComponent } from "./app.component";
-import { ROOT_REDUCER } from "./state/app.state";
+import { AppState, ROOT_REDUCER } from "./state/app.state";
 import { UsersEffects } from "./users/users.effects";
 import { NotifyEffects } from "./shared/notify/notify.effects";
 import { ThemeModule } from "./@theme/theme.module";
+import { localStorageSync } from "ngrx-store-localstorage";
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<AppState>
+): ActionReducer<AppState> {
+  return localStorageSync({ keys: ["auth"], rehydrate: true })(reducer);
+}
+const metaReducers: MetaReducer<AppState, any>[] = [localStorageSyncReducer];
 
 @NgModule({
   declarations: [AppComponent],
@@ -27,7 +35,7 @@ import { ThemeModule } from "./@theme/theme.module";
     HttpClientModule,
     RouterModule,
     NebularModule,
-    StoreModule.forRoot(ROOT_REDUCER),
+    StoreModule.forRoot(ROOT_REDUCER, { metaReducers }),
     ToastrModule.forRoot(),
     StoreDevtoolsModule.instrument({ maxAge: 10 }),
     EffectsModule.forRoot([UsersEffects, NotifyEffects]),
