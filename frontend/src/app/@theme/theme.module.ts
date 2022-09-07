b689ded0a02274601a1a74ec72b1784f53be0a37
Стив <kenethra.74@gmail.com>
2022-09-06 22:41:19 -0600

    feat(frontend): remove nebular module the theme

--- frontend/src/app/@theme/theme.module.ts
+++ frontend/src/app/@theme/theme.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { NebularModule } from "../nebular/nebular.module";
 
 @NgModule({
   declarations: [],
   exports: [],
-  imports: [CommonModule, NebularModule]
+  imports: [CommonModule]
 })
 export class ThemeModule {}
